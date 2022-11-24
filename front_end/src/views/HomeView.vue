e8f6a7238a16f0a4a4312324b0d05bd3f8a68cdc
Titouan MAGNIN <titouanmagnin2@gmail.com>
2022-11-24 18:13:15 +0100

    init partie vueJS + debut page show users + lien avec api fonctionnel

---
+++ front_end/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <showTable/>
+</template>
+
+<script>
+  // import HelloWorld from '../components/HelloWorld'
+  import showTable from '../components/showTable'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+        showTable,
+    },
+  }
+</script>
