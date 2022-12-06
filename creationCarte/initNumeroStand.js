a21d08edeeeecb560caa2337931a8e7924a4d5e5
Titouan MAGNIN <titouan.magnin@edu.univ-fcomte.fr>
2022-12-06 13:49:48 +0100

    maj carte

--- creationCarte/initNumeroStand.js
+++ creationCarte/initNumeroStand.js
@@ -2,37 +2,45 @@ import fs from "fs"
 import convert from "xml-js"
 const dataBuffer = fs.readFileSync('carte.svg')
 const data=convert.xml2js(dataBuffer, {compact: true, spaces: 4})
+// console.log(data)
 const aa=data.svg.g//g=aa=L calque - aa[i].a= L salle
 
 const CALQUE_STAND=0
 const listeSalle=aa[CALQUE_STAND].a
-let i,j,numSalle;
 aa[CALQUE_STAND]._attributes.id="listeStand";
-for (i in listeSalle){
-    numSalle=parseInt(i)+1
-    listeSalle[i]._attributes.id="salle"+numSalle
-    delete listeSalle[i]._attributes.style
-    for (j in listeSalle[i].rect){
-            // console.log(listeSalle[i].rect[j])
-        listeSalle[i].rect[j]._attributes.id=(numSalle*100+parseInt(j)).toString()
-        delete listeSalle[i].rect[j]._attributes.style
-        // listeSalle[i].rect[j]._attributes.style="z-index:1"
+delete listeSalle[CALQUE_STAND]._attributes["xlink:href"]
+for (let i=1;i<listeSalle.length;i++){
+    listeSalle[i]._attributes.id="salle"+i
+    // console.log(listeSalle[i].rect)
+    // console.log(typeof listeSalle[i].rect)
+    if (Array.isArray(listeSalle[i].rect)) {
+        delete listeSalle[i]._attributes.style
+        for (let j in listeSalle[i].rect) {
+            listeSalle[i].rect[j]._attributes.id = (parseInt(i) * 100 + parseInt(j)).toString()
+            delete listeSalle[i].rect[j]._attributes.style
+        }
+    }else{
+        listeSalle[i].rect._attributes.id=(parseInt(i)*100).toString()
     }
 }
 
+
 const calqueBatiment=aa[1-CALQUE_STAND]
-console.log(calqueBatiment.a)
-const listeBatiment=calqueBatiment.a.find(b=>b.rect.length==3)
+const listeBatiment=calqueBatiment.a.find(b=>{return "a" in b})//rect salle
+console.log(listeBatiment)
 listeBatiment._attributes.id="listeBatiment"
-console.log(listeBatiment.rect)
-for (const i in listeBatiment.rect) {
-    listeBatiment.rect[i]._attributes.id="batiment"+(parseInt(i)+1)
+delete listeBatiment._attributes.style
+delete listeBatiment._attributes["xlink:href"]
+calqueBatiment.a.find(b=>b.rect.length>10).rect.forEach(r=> {//passage
+    delete r._attributes.style
+})
+for (const i in listeBatiment.a) {
+    console.log(listeBatiment.a[i])
+    listeBatiment.a[i]._attributes.id="batiment"+(parseInt(i)+1)
+    listeBatiment.a[i].text.tspan._text="salle "+(parseInt(i)+1)
 }
 
-//     console.log(listeSalle[1]._attributes.id)
-//     console.log(listeSalle[1].rect[10]._attributes.id)
-// console.log(data.svg.g[1].a[1].rect[14]._attributes.id)
-// console.log(data.svg.g[1].a[1].rect)
+
 let file=convert.js2xml(data,{compact: true, spaces: 4})
 console.log(file)
 fs.writeFileSync("carteFormate.svg",file)
--- front_end/src/views/CarteView.vue
+++ front_end/src/views/CarteView.vue
@@ -1,29 +1,69 @@
 <template>
-    <CarteSVG @standSelected="action" :tabCouleur="tabCouleur" :modif-etat="elemActu"></CarteSVG>
+    <div>
+        <h1 style="text-align: center">Carte</h1>
+        <div style="display: flex">
+            <div style="width: 210mm; margin: 20px">
+                <CarteSVG @standSelected="action" :tabCouleur="data" :modif-etat="indexSelected"
+                          style="border: black 1px solid"></CarteSVG>
+            </div>
+            <div v-if="standSelected!==undefined"
+                 style="border: black 1px solid; min-width: 300px; margin: 20px; padding: 10px; height: 500px">
+                <h2 style="text-align: center">Stand : {{ standSelected.id }}</h2>
+                <div v-if="newStand">
+                    ...<br>
+                    ...<br>
+                    <v-btn color="grey" @click="createStand">Creer Stand</v-btn>
+                </div>
+                <div v-else>
+                    <p>Id : {{ standSelected.id }}</p>
+                    <p>Etat : {{ standSelected.couleur }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import CarteSVG from "../components/CarteSVG.vue"
+
 export default {
     name: "CarteView",
-    data:()=>{
-        return{
-            tabCouleur:Array(),
-            elemActu:{}
+    data: () => {
+        return {
+            standSelected: undefined,
+            indexSelected: -1,
+            data: [],
+            newStand: false
         }
     },
-    components:{CarteSVG},
-    methods:{
-        action(event){
-            let stand=this.tabCouleur.find(stand=>stand.id==event.id)
-            if (stand===undefined) {
-                this.tabCouleur.push({id: event.id.toString(), couleur: "blue"})
-                this.elemActu = {id: event.id, couleur:"blue"}
+    components: {CarteSVG},
+    methods: {
+        action(event) {
+            console.log("newStand : " + this.newStand)
+            if (this.newStand) {
+                this.standSelected.couleur = "black"
             }
-            else {
-                stand.couleur = "green"
-                this.elemActu = {id: event.id, couleur: stand.couleur}
-            }
+
+            let index = this.data.findIndex(stand => stand.id === event.id)
+            if (index === -1) {
+                this.newStand = true
+                index = this.data.length
+                this.data.push({id: event.id, couleur: ""})
+            } else if (this.data[index].couleur === "black")
+                this.newStand = true
+
+            let couleur = this.newStand ? "green" : "red"
+            this.data.splice(index, 1, {id: event.id, couleur: couleur})
+
+            console.log("aa", this.data.map(stand => stand.id))
+            // this.elemActu = {id: event.id, couleur: this.tabCouleur[event.id]}
+            this.standSelected = this.data.find(s => s.id === event.id);
+            this.indexSelected = index;
+        },
+        createStand() {
+            console.log("createStand")
+            this.newStand = false
+            this.action({id: this.standSelected.id})
         }
     },
 }
