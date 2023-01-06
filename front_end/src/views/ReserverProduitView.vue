a936c751fb5ad9b7401cb14e9b83fd15cbcc77f0
ataguia3 <ayoub.taguia@edu.univ-fcomte.fr>
2023-01-06 11:00:55 +0100

    fix

--- front_end/src/views/ReserverProduitView.vue
+++ front_end/src/views/ReserverProduitView.vue
@@ -1,14 +1,3 @@
-<!--<template>-->
-<!--  <form>-->
-<!--    <label>Quantité de produits à réserver :</label>-->
-<!--    <input v-model="quantite" type="number" min="1" required />-->
-<!--    <br />-->
-<!--    <label>Identifiant du produit à réserver :</label>-->
-<!--    <input v-model="idProduit" type="text" required />-->
-<!--    <br />-->
-<!--    <button @click.prevent="makeReservation">Réserver</button>-->
-<!--  </form>-->
-<!--</template>-->
 <template>
   <form class="form" @submit.prevent="makeReservation">
     <label for="quantite">Quantité de produits à réserver :</label>
@@ -31,7 +20,8 @@ export default {
   },
   methods: {
     async makeReservation() {
-      // Vérifier si la quantité de produits est disponible
+
+    // Vérifier si la quantité de produits est disponible
       // const available = await checkAvailability(this.idProduit, this.quantite);
       // if (!available) {
       //   Afficher un message d'erreur si la quantité de produits n'est pas disponible
@@ -39,17 +29,12 @@ export default {
         // return;
       // }
 
-      // Enregistrer la réservation en mettant à jour la base de données
       await this.saveReservation(this.idProduct, this.quantite);
-
-      // Afficher un message de confirmation
-      // this.showConfirmation('Votre réservation a bien été enregistrée');
 
       console.log("quantite", this.quantite);
       console.log("idProduit", this.idProduct);
       console.log("idStand", this.$route.params.idStand);
 
-      // Rediriger vers la boutique
       await this.$router.push({name: 'boutique', params: {idStand: this.$route.params.idStand}});
     },
     async saveReservation(idProduit, quantite) {
@@ -60,20 +45,12 @@ export default {
           idProduit: idProduit
         });
         console.log(response);
-        this.showConfirmation('Votre réservation a bien été enregistrée');
+        alert('Votre réservation a bien été enregistrée');
       } catch (error) {
         console.error(error);
-        this.showError('Une erreur est survenue lors de l\'enregistrement de votre réservation');
+        alert('Une erreur est survenue lors de l\'enregistrement de votre réservation');
       }
-    },
-    showError(message) {
-      // Afficher le message d'erreur à l'utilisateur
-      alert(message);
-    },
-    showConfirmation(message) {
-      // Afficher le message de confirmation à l'utilisateur
-      alert(message);
-    },
+    }
   }
 };
 </script>
